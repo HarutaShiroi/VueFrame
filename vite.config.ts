0005ab4bd768264585e83d6dc2d96b04169e6ec4
ShiroiHaruta <a790753643@hotmail.com>
2022-09-30 11:52:47 +0800

    22.9.30

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import pkg from "./package.json";
 import vue from "@vitejs/plugin-vue";
+
+/** 当前执行node命令时文件夹的地址（工作目录） */
+const root: string = process.cwd();
+
+/** 配置路径查找 */
+const pathResolve = (dir: string): string => {
+  return resolve(__dirname, ".", dir);
+};
+
+/** 设置路径别名 */
+const alias: Record<string, string> = {
+  "/@": pathResolve("src"),
+  "@build": pathResolve("build")
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   esbuild: {
-    drop: ["console", "debugger"],
-  },
+    drop: ["console", "debugger"]
+  }
 });
