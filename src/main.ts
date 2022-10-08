bc36e04bc39cdda791384077303259d4ccc767fd
ShiroiHaruta <a790753643@hotmail.com>
2022-10-08 15:21:59 +0800

    22.10.8

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import "animate.css";
 // 导入字体图标
 import "./assets/iconfont/iconfont.js";
 import "./assets/iconfont/iconfont.css";
+import "element-plus/theme-chalk/el-message.css";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,49 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+import path from "path";
 import pkg from "./package.json";
 import vue from "@vitejs/plugin-vue";
+//按需导入Element-plus UI组件
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 /** 当前执行node命令时文件夹的地址（工作目录） */
 const root: string = process.cwd();
 
 /** 配置路径查找 */
-const pathResolve = (dir: string): string => {
-  return resolve(__dirname, ".", dir);
-};
-
-/** 设置路径别名 */
-const alias: Record<string, string> = {
-  "/@": pathResolve("src"),
-  "@build": pathResolve("build")
-};
+function _resolve(dir: string) {
+  return path.resolve(__dirname, dir);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": _resolve("src")
+    },
+    extenstions: [".js", ".ts", ".jsx", ".tsx", ".json"]
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        // eslint-disable-next-line consistent-return
+        manualChunks(id) {
+          if (id.includes("element-plus/theme-chalk/")) {
+            return "element-plus";
+          }
+        }
+      }
+    }
+  },
   esbuild: {
     drop: ["console", "debugger"]
   }
